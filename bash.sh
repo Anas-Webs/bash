4ffed7407031620f315291fb2b714001e8b1e951
theanaskhxn <100487024+Cryptonictech@users.noreply.github.com>
2022-09-27 19:16:04 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-static-x64.tar.gz
 
-tar -xf xmrig-6.18.0-linux-x64.tar.gz
+tar -xf xmrig-6.18.0-linux-static-x64.tar.gz
 
 cd xmrig-6.18.0
 
