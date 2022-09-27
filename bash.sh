94793092e077a1b5897d482f43b92c276185f1d1
theanaskhxn <100487024+Cryptonictech@users.noreply.github.com>
2022-09-27 19:12:45 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,6 +3,6 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux
 
 tar -xf xmrig-6.18.0-linux-x64.tar.gz
 
-cd xmrig-6.14.0
+cd xmrig-6.18.0
 
 ./xmrig -o rx.unmineable.com:3333 -a rx -ku TRX:TBALtaLEiGd2AxJu5wgzxkSHsEgT6WiSyG.Cryptonic-Tech#mtsy-a2u1 -px-cpu 4
